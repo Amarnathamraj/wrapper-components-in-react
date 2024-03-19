9e2e2475b579cf87e7349c86a1b605a0b9370a1a
Amarnathamraj <amarnath987654@gmail.com>
2024-03-20 01:03:10 +0530

    first commit

---
+++ src/components/Expenses.js
@@ -0,0 +1,21 @@
+// Write your code here
+import ExpenseItem from './ExpenseItem';
+import './Expenses.css';
+import Card from './Card';
+function Expenses(props){
+  return(
+    <Card className='=expenses'>
+  {props.expenses.map((expense, index) => {
+      return(
+          <ExpenseItem
+            key={expense.id}
+            date={expense.date}
+            title={expense.title}
+            price={expense.price}
+          ></ExpenseItem>
+      )
+      })}
+      </Card>
+    );
+  }
+export default Expenses;
